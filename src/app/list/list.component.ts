44aa160d25858080b56274b4424d590f6c1c9a18
WellingtonAquino <waquino814@gmail.com>
2019-11-25 00:09:03 -0400

    updated the table

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Directive, Output, EventEmitter, ViewChildren, QueryList } from '@angular/core';
 import { Task } from '../models/Task';
 
 @Component({
@@ -7,36 +7,12 @@ import { Task } from '../models/Task';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
   @Input()
   data: Task[];
-  rowGroupMetadata: any;
   constructor() {
   }
-
   ngOnInit() {
-    this.updateRowGroupMetaData();
-  }
-  updateRowGroupMetaData() {
-    this.rowGroupMetadata = {};
-    if (this.data) {
-        for (let i = 0; i < this.data.length; i++) {
-            const rowData = this.data[i];
-            const category = rowData.category;
-            if (i === 0) {
-                this.rowGroupMetadata[category] = { index: 0, size: 1 };
-            } else {
-              const previousRowData = this.data[i - 1];
-              const previousRowGroup = previousRowData.category;
-                if (category === previousRowGroup) {
-                    this.rowGroupMetadata[category].size++;
-                } else {
-                    this.rowGroupMetadata[category] = { index: i, size: 1 };
-                }
-            }
-        }
-    }
-}
+  } 
 }
 
 
